4c8103e957c23e3f95844d441a85eb406e8c5a60
margrietcox <43604471+margrietcox@users.noreply.github.com>
2020-06-09 14:07:12 +0200

    Add files via upload

---
+++ PerformExperiments.py
@@ -0,0 +1,21 @@
+results = perform_experiments(LUSmodel, n_scenarios)
+    
+save_results(results, "02_6_10.tar.gz")
+
+experiments, outcomes = results
+ 
+    
+
+            
+    
+    
+    
+   
+    
+    
+    
+ 
+
+
+
+            
