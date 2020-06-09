a2fce59154bd9922aeee4dc643d5310f70cc036d
margrietcox <43604471+margrietcox@users.noreply.github.com>
2020-06-09 14:15:16 +0200

    Add files via upload

---
+++ LoadResults.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from ema_workbench import (save_results, load_results)
+
+results2 = load_results("02_6_10.tar.gz")
+print('resultsready')
+experiments, outcomes = results2
+ooi2 = outcomes['lusmap'].astype(int)
+    
+
+            
+    
+    
+    
+   
+    
+    
+    
+ 
+
+
+
+            
