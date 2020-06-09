01e5d2def9b7ee20ce7a589d6c042714f2e02ade
margrietcox <43604471+margrietcox@users.noreply.github.com>
2020-06-09 14:04:17 +0200

    Add files via upload

---
+++ ModelDefinition.py
@@ -0,0 +1,46 @@
+def LUS_model(TigrisXLfile, inbreiding, Density, Claimfile, natureclaim, recvalue, restrictionnatureweight, naturepolicymap, policyweightnature, spreidingratio, valuenaturelocation, pumping, agrclaim):
+    
+    changeresidentialclaim(TigrisXLfile, inbreiding, Density)
+    changeworkclaim(TigrisXLfile, Claimfile)
+    changenatureclaim(natureclaim)
+    changerecreationclaim(recvalue)
+    changeagricultureclaim(agrclaim)
+    
+    naturesuitability(valuenaturelocation, policyweightnature, pumping)
+    changehousingsuitability(spreidingratio)
+    changenaturepolicy(naturepolicymap, restrictionnatureweight)
+  
+    
+    
+    #runmodel
+    
+    config = notebook_dir+'/PD/cfg/stam.dms'
+    tree = '/EndResults/PerScenTimeUnit/STOOM_Y2012_Y2050/Run1'
+
+    geodmsrun(config, tree)
+    
+    
+    src = notebook_dir+'/PD/cfg/stam/results/run1.asc'
+    
+    lusmap = np.loadtxt(src, skiprows=6)
+    
+    dummy = np.max(lusmap)
+    
+    return lusmap, dummy
+
+ 
+    
+
+            
+    
+    
+    
+   
+    
+    
+    
+ 
+
+
+
+            
