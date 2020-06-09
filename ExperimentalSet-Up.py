bc8a9573f7eb31d78833edce20c5a3e36b9237de
margrietcox <43604471+margrietcox@users.noreply.github.com>
2020-06-09 15:07:15 +0200

    Update ExperimentalSet-Up.py

--- ExperimentalSet-Up.py
+++ ExperimentalSet-Up.py
@@ -1,8 +1,11 @@
 from ema_workbench import (RealParameter, IntegerParameter, BooleanParameter, ScalarOutcome, ArrayOutcome, Constant, Model, MultiprocessingEvaluator, Policy, perform_experiments, ema_logging )
-
 ema_logging.log_to_stderr(ema_logging.INFO)
+
+"""Define the model"""
     
 LUSmodel = Model('LUS', function=LUS_model)
+
+"""Define uncertainties"""
     
 LUSmodel.uncertainties = [  BooleanParameter('agrclaim'),
                             IntegerParameter('TigrisXLfile', 1, 4),
@@ -18,10 +21,11 @@ LUSmodel.uncertainties = [  BooleanParameter('agrclaim'),
                             IntegerParameter('valuenaturelocation', 1, 3),
                             BooleanParameter('pumping')]
                                     
-                               
+"""Define outcomes"""                              
                              
-
 LUSmodel.outcomes = [ArrayOutcome('lusmap')]
+
+"""Define number of scenarios"""
     
 n_scenarios = 10
  
