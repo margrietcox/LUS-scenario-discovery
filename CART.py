fcaad890bd9355f36818f18c27195fafcba01bbf
margrietcox <43604471+margrietcox@users.noreply.github.com>
2020-08-24 15:15:23 +0200

    Update CART.py

--- CART.py
+++ CART.py
@@ -1,7 +1,38 @@
-x = experiments
+
+clustering = AgglomerativeClustering(n_clusters=6, affinity='precomputed', linkage='complete').fit_predict(kappa_df)
+
+dfCART = experiments
 y = clustering
 
-cart_alg = cart.CART(x, y, mass_min = 0.5, mode = RuleInductionType.CLASSIFICATION)
+#Because CART uses a ratio scale by default, on-hot encoder is performed to the categorical parameters
+# in the experiments file:
+for scen in np.unique(dfCART['TigrisXLfile']):
+    dfCART['TigrisXLfile_{}'.format(scen)] = dfCART['TigrisXLfile']== scen
+    dfCART['TigrisXLfile_{}'.format(scen)] = dfCART['TigrisXLfile_{}'.format(scen)].astype(int)
+del dfCART['TigrisXLfile']
+
+for scen in np.unique(dfCART['natureclaim']):
+    dfCART['natureclaim_{}'.format(scen)] = dfCART['natureclaim']==scen
+    dfCART['natureclaim_{}'.format(scen)] = dfCART['natureclaim_{}'.format(scen)].astype(int)
+del dfCART['natureclaim']
+
+for scen in np.unique(dfCART['naturepolicymap']):
+    dfCART['naturepolicymap_{}'.format(scen)] = dfCART['naturepolicymap']==scen
+    dfCART['naturepolicymap_{}'.format(scen)] = dfCART['naturepolicymap_{}'.format(scen)].astype(int)
+del dfCART['naturepolicymap']
+
+for scen in np.unique(dfCART['valuenaturelocation']):
+    dfCART['valuenaturelocation_{}'.format(scen)] = dfCART['valuenaturelocation']==scen
+    dfCART['valuenaturelocation_{}'.format(scen)] = dfCART['valuenaturelocation_{}'.format(scen)].astype(int)
+del dfCART['valuenaturelocation']
+
+for scen in np.unique(dfCART['policyweightnature']):
+    dfCART['policyweightnature_{}'.format(scen)] = dfCART['policyweightnature']==scen
+    dfCART['policyweightnature_{}'.format(scen)] = dfCART['policyweightnature_{}'.format(scen)].astype(int)
+del dfCART['policyweightnature']
+
+
+cart_alg = cart.CART(dfCART, y, mode = RuleInductionType.CLASSIFICATION)
 
 
 cart_alg.build_tree()
